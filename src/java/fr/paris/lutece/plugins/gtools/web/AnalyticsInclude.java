5b1ee4d70f18b6cf3e0c9f7ccf884ad3db9685e6
pierrelevy <pierrelevy@users.noreply.github.com>
2013-04-27 22:37:10 +0000

    GTOOLS-1 : Initial upload
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-gtools@48009 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/gtools/web/AnalyticsInclude.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the description of 'Mairie de Paris' nor 'Lutece' nor the descriptions of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.gtools.web;
+
+import fr.paris.lutece.portal.service.content.PageData;
+import fr.paris.lutece.portal.service.datastore.DatastoreService;
+import fr.paris.lutece.portal.service.includes.PageInclude;
+import fr.paris.lutece.portal.service.plugin.Plugin;
+import fr.paris.lutece.portal.service.plugin.PluginService;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.util.html.HtmlTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Include of the ganalytics code
+ */
+public class AnalyticsInclude implements PageInclude
+{
+    private static final String KEY_ANALYTICS_CODE = "gtools.site_property.analytics.code";
+    private static final String MARK_GANALYTICS = "google_analytics";
+    private static final String PLUGIN_NAME = "gtools";
+    private static final String TEMPLATE_INCLUDE_GANALYTICS_CODE = "skin/plugins/gtools/include_analytics.html";
+    private static final String MARK_INCLUDE_GANALYTICS_CODE = "ganalytics_code";
+    private Plugin _plugin;
+
+    /**
+     * Substitue specific Freemarker markers in the page template.
+     * @param rootModel the HashMap containing markers to substitute
+     * @param data A PageData object containing applications data
+     * @param nMode The current mode
+     * @param request The HTTP request
+     */
+    @Override
+    public void fillTemplate( Map<String, Object> rootModel, PageData data, int nMode, HttpServletRequest request )
+    {
+        _plugin = PluginService.getPlugin( PLUGIN_NAME );
+
+        if( _plugin != null && request != null )
+        {
+	        Map<String, Object> model = new HashMap<String, Object>(  );
+	        String strCode = DatastoreService.getDataValue( KEY_ANALYTICS_CODE, "<no code provided>" );
+	        model.put( MARK_INCLUDE_GANALYTICS_CODE, strCode );
+	
+	        HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_INCLUDE_GANALYTICS_CODE,
+	                request.getLocale(  ), model );
+	
+	        rootModel.put( MARK_GANALYTICS, template.getHtml(  ) );
+        }
+        else {
+	        rootModel.put( MARK_GANALYTICS, "" );
+        }
+    }
+}
---
+++ src/sql/plugins/gtools/core/init_core_gtools.sql
@@ -0,0 +1,3 @@
+INSERT INTO core_datastore VALUES ('gtools.site_property.analytics.code', '<Your Google Analytics Code>');
+INSERT INTO core_datastore VALUES ('gtools.site_property.webmaster_tools.code', '<Your Webmaster Tools Code>');
+
